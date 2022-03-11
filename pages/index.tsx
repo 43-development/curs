1f36a4c7eead3bccbb6bdbde955bb8cb349f10bf
Alin Roman <alin.roman@pfh.ro>
2022-03-11 21:07:35 +0200

    simplificare cu js

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,72 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
+
+  const selector_unitate =
+    <select className="form-select">
+      <option>kg</option>
+      <option>l</option>
+      <option>buc</option>
+      <option>felii</option>
+      <option>plase</option>
+      <option>doze</option>
+    </select>
+  
+  const buton_stergere = <button className="btn link-danger"><i className="bi bi-x-circle"></i></button>
+
+  const adauga_produs =
+    <div className="produs">
+      <input type="text" className="form-control" />
+      <input type="button" value="adauga" className="btn btn-warning" />
+    </div>
+
+  const checkbox = <input type="checkbox" className="form-check-input" />
+  const cantitate = <input type="number" value="1" min="1" />
+
+  const denumire_produs = nume_produs => <span className="badge bg-success">{nume_produs}</span>
+  const denumire_magazin = nume_magazin => <div className="p-3 mb-2 bg-success text-white">{nume_magazin}</div>
+  const produs = nume_produs =>
+    <div className="produs">
+      {checkbox}
+      {denumire_produs(nume_produs)}
+      {cantitate}
+      {selector_unitate}
+      {buton_stergere}
+    </div>
+
+  const firma_luminoasa_magazin = nume_magazin =>
+    <div className="magazin titlu" data-bs-toggle="collapse" data-bs-target="#magazin-1">
+      {buton_stergere}
+      {denumire_magazin(nume_magazin)}
+    </div>
+
+  const cos_cumparaturi = (nume_produse: string[]) =>
+    <div className="cos collapse show" id="magazin-1">
+      {nume_produse.map(nume_produs => produs(nume_produs))}
+      {adauga_produs}
+    </div>
+
+  const magazin = (nume_magazin: string, nume_produse: string[]) =>
+    <div>
+      {firma_luminoasa_magazin(nume_magazin)}
+      {cos_cumparaturi(nume_produse)}
+    </div>
+  
+  const magazine = [
+    {
+      titlu: 'Mega Image',
+      produse: ["Carne", "Lapte", "Branza", "Faina", "Sampon", 'Banane']
+    },
+    {
+      titlu: 'Lidl',
+      produse: ['Bormasina', 'Jeleuri', 'Pasta', 'Paine', 'Cheie de 13']
+    },
+    {
+      titlu: 'Kaufland',
+      produse: ['Salata', 'Rosii']
+    }
+  ]
+
   return (
     <>
       <Head>
@@ -15,169 +81,9 @@ const Home: NextPage = () => {
           <input type="text" className="shadow p-3 mb-5 bg-white rounded" value="Lista mea" />
         </div>
         <div className="stores collapse show" id="stores">
-          <div>
-            <div className="magazin titlu" data-bs-toggle="collapse" data-bs-target="#magazin-1">
-              <button className="btn link-danger"><i className="bi bi-x-circle"></i></button>
-              <div className="p-3 mb-2 bg-success text-white">Mega Image</div>
-            </div>
-            <div className="cos collapse show" id="magazin-1">
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Carne</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#modal-1"><i className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Lapte</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#modal-2"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Branza</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#modal-3"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Faina</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#modal-3"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Sampon</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#modal-3"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Banane</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#modal-3"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="text" className="form-control" />
-                <input type="button" value="adauga" className="btn btn-warning" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="magazin titlu" data-bs-toggle="collapse" data-bs-target="#magazin-2">
-              <button className="btn link-danger"><i className="bi bi-x-circle"></i></button>
-              <div className="p-3 mb-2 bg-success text-white">Lidl</div>
-            </div>
-            <div className="cos collapse show" id="magazin-2">
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Bormasina</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#modal-4"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Jeleuri</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Pasta de dinti</span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="checkbox" className="form-check-input" />
-                <span className="badge bg-success">Paine </span>
-                <input type="number" value="1" min="1" />
-                <select className="form-select">
-                  <option>kg</option>
-                  <option>l</option>
-                  <option>buc</option>
-                  <option>felii</option>
-                  <option>plase</option>
-                </select>
-                <button className="btn link-danger" data-bs-toggle="modal" data-bs-target=".modal"><i
-                    className="bi bi-x-circle"></i></button>
-              </div>
-              <div className="produs">
-                <input type="text" className="form-control" />
-                <input type="button" value="adauga" className="btn btn-warning" />
-              </div>
-            </div>
-          </div>
+          {magazin('Mega Image', ["Carne", "Lapte", "Branza", "Faina", "Sampon", 'Banane'])}
+          {magazin('Lidl', ['Bormasina', 'Jeleuri', 'Pasta', 'Paine', 'Cheie de 13'])}
+          {magazin('Kaufland', ['Salata', 'Rosii'])}
           <div>
             <div className="magazin">
               <input type="text" />
