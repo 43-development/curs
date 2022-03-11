4f5125b7b534a6e85140de94e849b2f847cf89f3
irina <cristinaclipa@hotmail.com>
2022-03-11 21:24:32 +0200

    rezolvare conflicte

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-=======
->>>>>>> 1f36a4c7eead3bccbb6bdbde955bb8cb349f10bf
 
 const Home: NextPage = () => {
 
@@ -77,7 +75,6 @@ const Home: NextPage = () => {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"/>
->>>>>>> 1f36a4c7eead3bccbb6bdbde955bb8cb349f10bf
       </Head>
 =======
       <div className="foaie">
@@ -97,7 +94,6 @@ const Home: NextPage = () => {
         </div>
       </div>
     </>
->>>>>>> 1f36a4c7eead3bccbb6bdbde955bb8cb349f10bf
   )
 }
 
