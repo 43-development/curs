44fc73be5d5be78365859d4aedf63cc2d8d66fa4
christiannamissch <iam.notperfect@yahoo.com>
2022-04-01 21:22:46 +0300

    temaCristiana

--- javascript/tema cu functii.js
+++ javascript/tema cu functii.js
@@ -6,19 +6,20 @@ var functia1CareEProdusulArgumentelor = function  (argument1, argument2)
         return "produsul argumentelor este " + argument1 * argument2
     }
 
-var produsargumente = functia1CareEProdusulArgumentelor(10,11)
+functia1CareEProdusulArgumentelor(10,11)
 
 var functia2AdunareSiScadere = function (termen1, termen2, scazator) 
     {
         return "a = 5 , b = 6, c = 7, d= a+b - c. d=? " + termen1 + termen2 - scazator
     }
 
-var rezultat = functia2AdunareSiScadere (5,6,7)
+functia2AdunareSiScadere (5,6,7)
 
-var functia3 = function (arg1,arg2) 
+var functia3 = function (arg1, arg2) 
     {
-        var arg1 = functia2AdunareSiScadere - termen1 - termen2;    
-        return functia2AdunareSiScadere -termen1 -termen2 + "si" + arg2
+        var u = functia1CareEProdusulArgumentelor(arg1,arg2) 
+        var d = functia2AdunareSiScadere (arg1, arg2, u)    
+        return  d
     }
  var rezultat_complicat= functia3(7,1)
  var rezultat_complicat2 = functia3(14,6)
@@ -28,22 +29,21 @@ var functia3 = function (arg1,arg2)
 
 function functia1CareEProdusulArgumentelor(argument1,argument2) 
     {
-        Produs = argument1 * argument2;
-        return produs
+        return  argument1 * argument2;   
     }
 
 functia1CareEProdusulArgumentelor(10,11)
 
-function functia2AdunareSiScadere(a,b,c)
+function functia2AdunareSiScadere(a, b, c)
     {
-        return "a= 5, b= 6, c= 7, a+b=  " + a+b + "a+b-c="+  a + b - c
+        return  "rezultatul este" + a+b-c
     }
+functia2AdunareSiScadere(5, 6, 7)
 
 function functia3complicata(m, n)
     {   
-        var functia1CareEProdusulArgumentelor = argument3
-        var m = functia2AdunareSiScadere(termen1, termen2, argument3)
-        return m +"si"+ n 
+       
+        return functia2AdunareSiScadere(m,n, functia1CareEProdusulArgumentelor(m,n))
     }
 
 var primul_rezultat= functia3complicata(7,1)
@@ -52,8 +52,8 @@ var rezultatul_numerelor_alese_de_mine = functia3complicata(10,5)
 
 //sau incercarea cu console.log 
 
-console.log("Prima incercare este"+ functia3complicata(7,1))
-console.log("a doua incercare este"+ functia3complicata (14,6))
+console.log("Prima incercare este" + functia3complicata(7,1))
+console.log("a doua incercare este" + functia3complicata (14,6))
 console.log("ultima incercare este" + functia3complicata (10,5))
 
 // dă, Doamne, să fie bine
