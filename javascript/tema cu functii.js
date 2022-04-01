0be6476c6ec27c3e5fec5740d0f013d97221760e
christiannamissch <iam.notperfect@yahoo.com>
2022-04-01 09:35:50 +0300

    TemaCuFunctii

---
+++ javascript/tema cu functii.js
@@ -0,0 +1,58 @@
+var produs = function (factor1, factor2){
+    return "rezultatul este " + factor1 * factor2
+}
+var ceva = produs (2,5)
+produs (3,4)
+
+// de aici incepe tema 
+
+var functia1CareEProdusulArgumentelor = function  (argument1, argument2)  
+{
+return "produsul argumentelor este" + argument1 * argument2
+}
+
+var functia2AdunareSiScadere = function (termen1, termen2, scazator) 
+{
+    return "a = 5 , b = 6, c = 7, d= a+b - c. d=? " + termen1 + termen2 - scazator
+}
+functia2AdunareSiScadere (5,6,7)
+
+
+var functia3 = function (arg1,arg2) {
+    var arg1 = functia2AdunareSiScadere - termen1 - termen2;
+    
+    return functia2AdunareSiScadere -termen1 -termen2 + "si" + arg2
+}
+
+// a doua incercare
+
+function functia1CareEProdusulArgumentelor(argument1,argument2) 
+{
+    Produs = argument1 * argument2;
+    return produs
+ }
+
+    functia1CareEProdusulArgumentelor(10,11)
+
+function functia2AdunareSiScadere(a,b,c)
+{
+return "a= 5, b= 6, c= 7, a+b=  " + a+b + "a+b-c="+  a + b - c
+}
+
+
+
+
+function functia3complicata(m, n)
+{   var functia1CareEProdusulArgumentelor = argument3
+    var m = functia2AdunareSiScadere(termen1, termen2, argument3)
+ return m +"si"+ n
+   
+}
+functia3complicata()
+
+console.log("Prima incercare este"+ functia3complicata(7,1))
+console.log("a doua incercare este"+ functia3complicata (14,6))
+console.log("ultima incercare este" + functia3complicata (10,5))
+
+// dă, Doamne, să fie bine
+
